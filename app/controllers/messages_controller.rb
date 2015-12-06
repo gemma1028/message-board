5bb171a242aec91749cf0587ab0575040ffa0858
gemma1028 <hideki.suzuki@social-bridge.com>
2015-12-06 20:48:41 +0000

    Gemfile update for heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,27 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]  
   def index
     # Messageを全て取得する。
     @messages = Message.all
     @message = Message.new
+  end
+
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
 
   ## ここから追記
@@ -17,10 +36,14 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
-  ## ここまで
   
+  private
   def message_params
     params.require(:message).permit(:name, :body)
   end
+  ## ここまで
   
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
