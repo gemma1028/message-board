ebf5cd538e4e2650b98d8f2e79038b367decca42
gemma1028 <hideki.suzuki@social-bridge.com>
2015-12-09 18:31:17 +0000

    challenge2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -24,7 +24,7 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to root_path , notice: 'メッセージを保存しました'
+      redirect_to root_path, notice: 'メッセージを保存しました'
     else
       # メッセージが保存できなかった時
       @messages = Message.all
